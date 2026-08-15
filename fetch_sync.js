b0bbe54ba586c843d5081ec0627c270784bee4a7
Jalpa <jalpa.crayon@gmail.com>
2026-08-15 16:12:21 +0530

    Product table remove & changes regarding to it

--- fetch_sync.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'postgresql://inkreatix:inkreatix@194.163.134.149:3455/inkreatix' });
-async function run() {
-  await client.connect();
-  const res = await client.query("SELECT * FROM sync_history ORDER BY \"created_at\" DESC LIMIT 5;");
-  console.log(JSON.stringify(res.rows, null, 2));
-  await client.end();
-}
-run().catch(console.error);
